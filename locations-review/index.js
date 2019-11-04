2e1b1d5d3936248516e347f8bf057585a375f199
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-11-04 14:53:50 +0530

    updating

--- locations-review/index.js
+++ locations-review/index.js
@@ -2,13 +2,13 @@ var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
 
-dust.loadSource(dust.compile(require('./template.html'), 'admin-client-contacts-review'));
-dust.loadSource(dust.compile(require('./actions.html'), 'admin-client-contacts-review-actions'));
+dust.loadSource(dust.compile(require('./template.html'), 'admin-client-locations-review'));
+dust.loadSource(dust.compile(require('./actions.html'), 'admin-client-locations-review-actions'));
 
 var find = function (id, done) {
     $.ajax({
         method: 'GET',
-        url: utils.resolve('accounts:///apis/v/contacts/' + id),
+        url: utils.resolve('accounts:///apis/v/locations/' + id),
         dataType: 'json',
         success: function (data) {
             done(null, data);
@@ -25,31 +25,31 @@ module.exports = function (ctx, container, options, done) {
             return done(err);
         }
         var sandbox = container.sandbox;
-        dust.render('admin-client-contacts-review', serand.pack(contact, container), function (err, out) {
+        dust.render('admin-client-locations-review', serand.pack(contact, container), function (err, out) {
             if (err) {
                 return done(err);
             }
             sandbox.append(out);
 
-            $('.contact-ok', sandbox).on('click', function () {
+            $('.location-ok', sandbox).on('click', function () {
                 var thiz = $(this);
                 utils.loading();
-                utils.publish('accounts', 'contacts', contact, function (err) {
+                utils.publish('accounts', 'locations', contact, function (err) {
                     utils.loaded();
                     if (err) {
                         return console.error(err);
                     }
                     thiz.removeClass('text-primary').addClass('text-success')
-                        .siblings('.contact-bad').addClass('hidden');
+                        .siblings('.location-bad').addClass('hidden');
 
                     setTimeout(function () {
-                        serand.redirect('/manage-contacts');
+                        serand.redirect('/manage-locations');
                     }, 500);
                 });
             });
 
             done(null, function () {
-                $('.admin-client-contacts-review', sandbox).remove();
+                $('.admin-client-locations-review', sandbox).remove();
             });
         });
     });
